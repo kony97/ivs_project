6ff159ea4121538924535200a03e6b0eca41aa4c
Jaromír <jaromirstastny.99@seznam.cz>
2021-04-24 15:24:14 +0200

    test

--- Math.Library/Math.cs
+++ Math.Library/Math.cs
@@ -4,44 +4,57 @@ namespace Math.Library
 {
     public class Math : IMath
     {
-        public double Abs(double n1, double n2)
+        public double Abs(double n1)
         {
-            throw new NotImplementedException();
+            if (n1 < 0) return -n1;
+            return n1; 
         }
 
-        public double Add(double n1, double n2)
+        public double Add(double n1, double n2) //sčítání
         {
-            throw new NotImplementedException();
+            return n1 + n2;
         }
 
-        public double Div(double n1, double n2)
+        public double Div(double n1, double n2) //dělení
         {
-            throw new NotImplementedException();
+            return n1 / n2;
         }
 
-        public long Fact(int n)
+        public long Fact(int n) //faktoriál
         {
-            throw new NotImplementedException();
+            int factorial = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
         }
 
-        public double Mul(double n1, double n2)
+        public double Mul(double n1, double n2) //násobení
         {
-            throw new NotImplementedException();
+            return n1 * n2;
         }
 
-        public double Pow(double n1, double n2)
+        
+        public double Pow(double n1, double n2) //N1 umocněno na N2
         {
-            throw new NotImplementedException();
+          
+            for(int i = 0; i < n2; i++)
+            {
+                n1 *= n1;
+            }
+            return n1;
         }
 
-        public double Sqrt(double n1, double n2)
+        public double Sqrt(double n1, double n2) //odmocnina
         {
-            throw new NotImplementedException();
+            //TODO
+            return n1;
         }
 
-        public double Sub(double n1, double n2)
+        public double Sub(double n1, double n2)//odčítá N2 od N1 a vrací N1
         {
-            throw new NotImplementedException();
+            return n1 - n2;
         }
     }
 }
