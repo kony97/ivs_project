060a15bb6d683e781a318ab8ee8883f6ec436366
Jakub Konetzný <xkonet01@vutbr.cz>
2021-04-24 14:11:55 +0200

    interface

---
+++ Math.Library/IMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math.Library
+{
+    interface IMath
+    {
+        double Add(double n1, double n2);
+
+        double Sub(double n1, double n2);
+
+        double Mul(double n1, double n2);
+
+        double Div(double n1, double n2);
+
+        double Abs(double n1, double n2);
+
+        long Fact(int n);
+
+        double Sqrt(double n1, double n2);
+
+        double Pow(double n1, double n2);
+    }
+}
---
+++ Math.Library/Math.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Math.Library
+{
+    public class Math : IMath
+    {
+        public double Abs(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Add(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Div(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Fact(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Mul(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Pow(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sqrt(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sub(double n1, double n2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
