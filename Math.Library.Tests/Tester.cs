e238b6f11c14aebe8ce99e5e15527e93ec48dfa2
Jakub Konetzný <xkonet01@vutbr.cz>
2021-04-24 13:55:22 +0200

    přidání knihovny a testu

---
+++ Math.Library.Tests/Tester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Math.Library.Tests
+{   [TestFixture]
+    public class Tester
+    {
+        [Test]
+        public void simpleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Math.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Math.Library
+{
+    public class Class1
+    {
+    }
+}
